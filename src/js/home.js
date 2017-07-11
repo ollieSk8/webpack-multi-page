1b23be1e7c59efcfc43f6b7b05f6c6308b228dc9
ollieSk8 <412537738@qq.com>
2017-07-11 22:43:07 +0800

    upload img

--- src/js/home.js
+++ src/js/home.js
@@ -1,37 +1,54 @@
 import Vue from 'vue';
-import Quill from 'quill';
+import {Upload} from 'element-ui';
+import VeeValidate from 'vee-validate';
+Vue.use(VeeValidate);
+Vue.use(Upload)
 new Vue({
-	data(){
-		return {
-			html:'',
-			content:''
-		}
-	},
-	mounted(){
-	  var _this=this;
-	  var toolbarOptions = [
-	    [{ 'header': [1, 2, 3, false] }],
-	  	['bold', 'italic', 'underline', 'strike']
+	el:'#app',
+	data() {
+      return {
+        imageUrl: '',
+        imageUrlBol:false,
+        isImageType:false,
+        isImageSize:false
+      };
+    },
+    methods: {
+      handleAvatarSuccess(res, file) {
+      	this.isImageType=false;
+        this.isImageSize=false;
+        this.imageUrlBol=false;
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
 
-	  ];
-	  var options = {
-		  // debug: 'info',
-		  placeholder: 'Compose an epic...',
-		  theme: 'snow',
-		  modules: {
-		    toolbar: toolbarOptions
-		  }
-     };
-
-     var quill = new Quill('#editor',options);
-	  quill.on('text-change', function(delta, oldDelta, source) {
-		 if (source == 'user') {
-		 	
-		   _this.html=quill.root.innerHTML
-		   //var delta = quill.getContents();
-		   _this.content=quill.container.firstChild.innerHTML
-		   console.log(_this.html)
-		 }
-	  });
-	}
-}).$mount('#app');
+        if (!isJPG) {
+          this.isImageType=true;//格式错误信息展示
+          this.imageUrlBol=false;//是否被需要隐藏
+          this.isImageSize=false;//大小限制隐藏
+        }
+        if (!isLt2M) {
+          this.isImageType=false;
+          this.isImageSize=true;
+          this.imageUrlBol=false;
+        }
+        return isJPG && isLt2M;
+      },
+      validate() {
+	      this.$validator.validateAll().then(result => {
+	          if(this.imageUrl==''){
+	          	this.imageUrlBol=true;
+	          	this.isImageType=false;
+	          	this.isImageSize=false;
+	          }else{
+	          	this.imageUrlBol=false;
+	          }
+	      });
+	  }
+    },
+    components:{
+    	'el-upload':Upload
+    }
+})
